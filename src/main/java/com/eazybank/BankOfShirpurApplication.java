fbf81448d1b012067fa5c8fa5df915c13215d391
bhushan <bhushanshimpi2816@gmail.com>
2022-11-17 23:20:02 +0530

    adding project to Repo

---
+++ src/main/java/com/eazybank/BankOfShirpurApplication.java
@@ -0,0 +1,13 @@
+package com.eazybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfShirpurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfShirpurApplication.class, args);
+	}
+
+}
